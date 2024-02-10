a1f63dbec2732809fd03e3ffb354c678999f550b
naoya25 <onaoya2002@gmail.com>
2024-02-10 23:37:28 +0900

    fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
-
-const Footer = (props: Props) => {
+const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="w-[100vw] bg-black text-white">
+      <div className="flex justify-between">
+        <div className="m-10">
+          <p className="text-[2em]">Auther's</p>
+          <p>Yuki</p>
+          <p>Mone</p>
+          <p>Nasu</p>
+          <p>Naoya</p>
+        </div>
+        <div className="m-10">
+          <p className="text-[2em]">Team</p>
+          <p className="text-[3em]">史上最強コミュニティ</p>
+        </div>
+        <div className="m-5 text-[1.5em]">
+          <p>AUTHENTIC</p>
+          <p>BEAUTY</p>
+          <p>CONCEPT</p>
+        </div>
+      </div>
+      <p className="flex justify-center">&copy; 2024 Your Company Name. All rights reserved.</p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
