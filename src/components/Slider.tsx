06d3c1b4ace5aa707304624016e04f91285d6456
naoya25 <onaoya2002@gmail.com>
2024-02-11 02:43:37 +0900

    add カルーセルスライダー

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/css";
 
-type Props = {}
+const images = [
+  "slider/slider1.jpg",
+  "slider/slider2.jpg",
+  "slider/slider3.jpg",
+];
 
-const Slider = (props: Props) => {
+const Slider = () => {
   return (
-    <div>Slider</div>
-  )
-}
+    <div className="slider">
+      <Splide
+        aria-label="私のお気に入りの画像集"
+        options={{
+          autoplay: true, // 自動再生を有効
+          interval: 3000, // 自動再生の間隔を3秒に設定
+        }}
+      >
+        {images.map((src) => (
+          <SplideSlide>
+            <img
+              className="slide-img block w-[90vw] h-[90vh] mx-auto"
+              style={{ objectFit: "cover" }}
+              src={src}
+              alt="no image"
+            />
+          </SplideSlide>
+        ))}
+      </Splide>
+    </div>
+  );
+};
 
-export default Slider
+export default Slider;
