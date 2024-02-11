a363cc0114f86b75b1976b0141059449e2ebbeec
naoya25 <onaoya2002@gmail.com>
2024-02-11 11:47:38 +0900

    add comment

--- src/app/Post/[id]/page.tsx
+++ src/app/Post/[id]/page.tsx
@@ -7,7 +7,7 @@ import { useEffect, useState } from "react";
 
 function PlanDetails() {
   const params = useParams();
-  const [plan, setPlan] = useState<PlanData | null>(null);
+  const [plan, setPlan] = useState<any>(null);
   const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 
   useEffect(() => {
@@ -16,7 +16,7 @@ function PlanDetails() {
         const response = await fetch(`${baseUrl}/plans`);
         const jsonData = await response.json();
         console.log(jsonData);
-        const allPlans = jsonData.plans;
+        const allPlans = jsonData;
         const matchingPlan = allPlans.find(
           (p: PlanData) => p.id == Number(params.id)
         );
@@ -33,10 +33,38 @@ function PlanDetails() {
     fetchData();
   }, []);
 
+  async function postData(url = "", data = {}) {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+
+    const response = await fetch(url, options);
+    return response.json();
+  }
+
+  const [star, setStar] = useState<number>(1);
+  const [commentBody, setCommentBody] = useState<string>("");
+  const commentSubmit = () => {
+    postData(`${baseUrl}/comment/${plan.id}`, {
+      comment: commentBody,
+      stars: star,
+    })
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
   return (
     <div className="min-h-[80vh] w-[100vw] flex p-24">
       <div className="w-[40%]">
-        <p className="text-[5em] font-bold">Title ~~~~~~~</p>
+        <p className="text-[5em] font-bold">Title: {plan?.title}</p>
         <p className="m-2">Stars</p>
         <p className="m-2">おすすめパートナー：{plan?.with_whom}</p>
         <div className="h-2 w-[95%] mx-auto bg-black rounded-[4px]"></div>
@@ -44,6 +72,37 @@ function PlanDetails() {
         <p className="m-5">{plan?.description}</p>
         <p className="m-2 text-[1.5em]">Situation</p>
         <p className="m-5">{plan?.situation}</p>
+        <div className="comment">
+          <p>新規コメント</p>
+          <form onSubmit={commentSubmit}>
+            <div>
+              <label>星:</label>
+              <input
+                type="number"
+                value={star}
+                onChange={(e) => setStar(Number(e.target.value))}
+                min="1"
+                max="5"
+              />
+            </div>
+            <div>
+              <label>コメント:</label>
+              <input
+                type="text"
+                value={commentBody}
+                onChange={(e) => setCommentBody(e.target.value)}
+              />
+            </div>
+            <button type="submit">コメントする</button>
+          </form>
+          <p>コメント一覧</p>
+          {plan?.comments.map((e: any) => (
+            <p>
+              {e.stars}
+              {e.comment}
+            </p>
+          ))}
+        </div>
       </div>
       <div className="w-[60%] p-10">
         <p className="text-[3em] font-bold">Places</p>
